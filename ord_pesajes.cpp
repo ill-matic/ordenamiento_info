ad3d2b75b10e4611b0820e6f23bfaa1c07094aac
matias <107221709+ill-matic@users.noreply.github.com>
2022-06-09 19:14:43 -0300

    Add files via upload

---
+++ ord_pesajes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a[99];
+	int i=0,aux =0, pos = 0,cant=0;
+	printf("\t----PROGRAMA DE ORDENAMIENTO DE PESOS----\n\n");
+	printf("Ingrese la cantidad de peleadores:\n");
+	scanf("%d",&cant);
+	printf("Ingrese el peso en kilogramos de los peleadores\n");
+	for(i=0; i<cant ; i++){
+		scanf("\t%d",&a[i]);
+	}
+	for (i=0;i<cant;i++){
+		aux = a[i];
+		pos = i;
+		while((pos>0)&&(aux < a[pos-1])){
+			a[pos]=a[pos-1];
+			pos--;
+		}
+		a[pos]=aux;
+	}
+	printf("Pesajes en orden acendente:\n");
+	for(i=0;i<cant;i++){
+		printf("\t%d",a[i]);
+	}
+	printf("\nPesajes en orden decendente:\n");
+	for(i=cant-1;i>=0;i--){
+		printf("\t%d",a[i]);
+	}
+	return 0;
+}
+
---
+++ ord_por_incercion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	/**cantidad de elementos puede variar**/
+	int a[5]{1,2,3,4,5};
+	int i=0,aux =0, pos = 0;
+	
+	for (i=0;i<5;i++){
+		aux = a[i];
+		pos = i;
+		while((pos>0)&&(aux < a[pos-1])){
+			a[pos]=a[pos-1];
+			pos--;
+		}
+		a[pos]=aux;
+	}
+	printf("En orden acendente:\n");
+	for(i=0;i<5;i++){
+		printf("\t%d",a[i]);
+	}
+	printf("\nEn orden decendente:\n");
+	for(i=4;i>=0;i--){
+		printf("\t%d",a[i]);
+	}
+	return 0;
+}
+
